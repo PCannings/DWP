be09ebdae40b81e97125d83db6886ea9b2ef2da9
Calum Murray <Cal_92@hotmail.co.uk>
2013-09-11 12:05:53 +0100

    Main activity logic in place

--- JobSeeker/src/itp/team1/jobseeker/MainActivity.java
+++ JobSeeker/src/itp/team1/jobseeker/MainActivity.java
@@ -3,19 +3,68 @@ package itp.team1.jobseeker;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity 
+{
 
+	private ImageView banner;
+	private Button 	  searchButton;
+	private Button 	  diaryButton;
+	
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState) 
+	{
+		// Setup UI
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main_activity);
+		
+		// Get buttons
+		banner 		 = (ImageView) findViewById(R.id.banner);
+		searchButton = (Button) findViewById(R.id.search_button);
+		diaryButton  = (Button) findViewById(R.id.diary_button);
+
+		
+		// Banner Button event
+		banner.setOnClickListener(new View.OnClickListener() 
+		{
+			@Override
+			public void onClick(View v) 
+			{
+				Toast.makeText(MainActivity.this, "Banner clicked", Toast.LENGTH_SHORT).show();
+			}
+		});
+		
+		// Search Button event
+		searchButton.setOnClickListener(new View.OnClickListener() 
+		{
+			@Override
+			public void onClick(View v) 
+			{
+				Toast.makeText(MainActivity.this, "Search clicked", Toast.LENGTH_SHORT).show();
+
+			}
+		});
+		
+		// Diary Button event
+		diaryButton.setOnClickListener(new View.OnClickListener() 
+		{
+			@Override
+			public void onClick(View v) 
+			{
+				Toast.makeText(MainActivity.this, "Diary clicked", Toast.LENGTH_SHORT).show();
+			}
+		});
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
+		getMenuInflater().inflate(R.menu.main_menu, menu);
 		return true;
 	}
 
