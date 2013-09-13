51c4f719ece29884d806969f34e38e015f374be4
Calum Murray <Cal_92@hotmail.co.uk>
2013-09-13 14:29:32 +0100

    HTTP POST Client added - BROKEN

--- JobSeeker/src/itp/team1/jobseeker/SearchActivity.java
+++ JobSeeker/src/itp/team1/jobseeker/SearchActivity.java
@@ -1,7 +1,30 @@
 package itp.team1.jobseeker;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.StringBufferInputStream;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.entity.mime.MultipartEntity;
+import org.apache.http.entity.mime.content.InputStreamBody;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.params.HttpParams;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
 import android.net.ConnectivityManager;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.app.Activity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -17,6 +40,9 @@ import android.os.Build;
 
 public class SearchActivity extends Activity 
 {
+	// TODO: Deploy to AWS (http://ec2-54-241-217-77.us-west-1.compute.amazonaws.com:8080/JobSeekerServer/search)
+	private static final String JOB_SEEKER_SERVER = "http://localhost:8080/JobSeekerServer/search";
+	
 	private EditText titleField;
 	private EditText employerField;
 	private Spinner  typeField;
@@ -48,22 +74,77 @@ public class SearchActivity extends Activity
 			@Override
 			public void onClick(View v) 
 			{
+				List<Job> jobResults = null;
 				// TODO: Go to server to scrape all sources	
 				// TODO: ASyncTask progress bar
 				
 			    // Check for Internet access
-//                if (!hasInternetConnectivity())
-//                {
-//                    Toast.makeText(SearchActivity.this, "Internet Access Required...", Toast.LENGTH_LONG).show();
-//                    return;
-//                }
+                if (!hasInternetConnectivity())
+                {
+                    Toast.makeText(SearchActivity.this, "Internet Access Required...", Toast.LENGTH_LONG).show();
+                    return;
+                }
 
-				
+    			try 
+    			{
+                    // Send ("POST") search data to server
+                    HttpPost postMessage = createPostMessage();
+        			HttpClient httpClient = new DefaultHttpClient();
+
+					HttpResponse response = httpClient.execute(postMessage);
+					
+	    			// Parse JSON return for Job results
+					jobResults = parseJSONResponse(response);
+
+				} 
+    			catch (ClientProtocolException e) 
+    			{
+					System.out.println("ClientPrototcolException: Can't send POST data to server");
+				} 
+    			catch (IOException e) 
+    			{
+					System.out.println("IOException: Can't send POST data to server");
+				}
+
+    			Intent intent = new Intent(SearchActivity.this, ResultsActivity.class);
+    			intent.putParcelableArrayListExtra("jobResults", (ArrayList<? extends Parcelable>) jobResults);
 				startActivity(new Intent(SearchActivity.this, ResultsActivity.class));
 			}
 		});
 	}
 
+	/**
+	 * Sets up the post request
+	 */
+	@SuppressWarnings("deprecation")
+	private static HttpPost createPostMessage(/*Search searchParameters*/) 
+	{
+		HttpPost postRequest = new HttpPost(JOB_SEEKER_SERVER);
+		
+		HttpParams params = new BasicHttpParams();
+		HttpConnectionParams.setConnectionTimeout(params, 10000);
+		HttpConnectionParams.setSoTimeout(params, 30000);
+		postRequest.setParams(params);
+		
+		// Specify Content and Content-Type parameters for POST request
+		InputStream dataIn = new StringBufferInputStream("Test Data");// TODO: searchParameters
+		@SuppressWarnings("deprecation")
+		MultipartEntity entity = new MultipartEntity();
+		entity.addPart("Content", new InputStreamBody(dataIn, "Content"));
+		postRequest.setEntity(entity);
+		
+		return postRequest;
+	}
+	
+	private static List<Job> parseJSONResponse(HttpResponse response) throws IOException 
+	{
+		Gson gson = new Gson();
+		InputStreamReader isr = new InputStreamReader(response.getEntity().getContent());
+        Type PostListings = new TypeToken<List<Job>>() {}.getType();
+		List<Job> jobResults = gson.fromJson(isr, PostListings);
+		return jobResults;
+	}
+	
 	// Check for Internet access
 	public boolean hasInternetConnectivity() 
 	{
